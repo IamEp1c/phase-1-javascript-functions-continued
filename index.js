abdddc49fc8032c6a7d01be53138f2a3d49fa8aa
Waqas Anjum <Qanjum96@gmail.com>
2022-04-05 18:07:55 -0400

    commit tree 44a4442ac2473ef40c578e9296d5b94c3f52f0aa

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(string = 'roller-skate'){
+    return `This Saturday, I want to ${string}!`
+}
+
+const mondayWork = (string = 'go to the office') => {
+    return `This Monday, I will ${string}.`
+}
+
+function wrapAdjective(string = '*'){
+    return function result(string2 = '||'){
+        return `You are ${string}${string2}${string}!`
+    }
+
+}
+
+// wrapAdjective(arguments passed here)
